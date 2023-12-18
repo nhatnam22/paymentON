99cefee6ea94eb0a7053dfff135efeaed29a996f
nhatnam22 <nanana2a4e@gmail.com>
2023-12-18 17:12:18 +0700

    up all

---
+++ src/main/java/com/projectpayment/OnlinePayApplication.java
@@ -0,0 +1,13 @@
+package com.projectpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePayApplication.class, args);
+	}
+
+}
